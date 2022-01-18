2b602529a8085e7799aa86fa2a8ce82cffbe529e
VertoDebru <t.vervoot@gmail.com>
2022-01-18 21:26:15 +0100

    Update routes & modules.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
+
 const userRoutes = require('./routes/user');
 const saucesRoutes = require('./routes/sauces');
 
@@ -9,14 +11,14 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Cross-Origin-Resource-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
 
     next();
 });
 
 app.use(bodyParser.json());
-app.post('/api/auth/login', userRoutes);
-app.post('/api/auth/signup', userRoutes);
-app.get('/api/sauces', saucesRoutes);
+app.use('/api/auth/', userRoutes);
+app.use('/api/sauces', saucesRoutes);
+app.get('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
