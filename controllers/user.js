839feb5744ce22cf6b7e0992011a25fe9256a914
VertoDebru <t.vervoot@gmail.com>
2022-01-18 21:24:41 +0100

    Add bcrypt & token.

--- controllers/user.js
+++ controllers/user.js
@@ -1,44 +1,52 @@
+const bCrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.userSign = (req,res) => {
     if (!req.body.email || !req.body.password) {
-        return res.status(400).send(new Error('Bad request!'));
+        return res.status(400).json({ error: 'Empty input!' });
     }
-    console.log("New Sign up.");
-    console.log(req.body.email + req.body.password);
-    
-    User.findOne({ email: req.body.email })
-    .then( user => {
-        if(user) {
-            res.status(401).json({ message: 'Email existant!'});
-        }
-        else {
-            console.log('New registery.');
-            let user = new User({
-                ...req.body
-            });
-            user.save()
-            .then(() => res.status(200).json({ userId: user._id, token: 'tokenExample' }))
-            .catch(error => res.status(400).json({ error }));
-        }
+    console.log("New Sign up for :" + req.body.email);
+    bCrypt.hash(req.body.password, 10)
+    .then( hash => {
+        console.log('New registery.');
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        });
+        user.save()
+        .then(() => res.status(201).json({ 
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', { expiresIn: '24h' })
+        }))
+        .catch(error => res.status(400).json({ error }));
     })
-    .catch(error => res.status(401).json({ error }));
+    .catch(error => res.status(500).json({ error }));
 };
 
 exports.userLogin = (req,res) => {
     if (!req.body.email || !req.body.password) {
-        return res.status(400).send(new Error('Bad request!'));
+        return res.status(400).json({ error: 'Empty input!' });
     }
     console.log("New login : " + req.body.email);
     User.findOne({ email: req.body.email })
     .then( user => {
-        console.log(user.password);
-        if(user.password == req.body.password) {
-            res.status(200).json({ userId: user._id, token: 'tokenExample' });
-        }
-        else {
-            res.status(401).json({ message: 'Mot de passe incorrect!'});
-        }
+        if(!user) return res.status(401).json({ error: 'Utilisateur inexistant!' });
+        
+        bCrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            if(!valid) return res.status(401).json({ error: 'Mot de passe incorrect!' });
+
+            res.status(200).json({
+                userId: user._id,
+                token: jwt.sign(
+                    { userId: user._id },
+                    'RANDOM_TOKEN_SECRET',
+                    { expiresIn: '24h' }
+                )
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
     })
     .catch(error => res.status(401).json({  message: 'Email inconnu!' }));
 };
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const ctrlUser = require('../controllers/user');
 
-router.post('/api/auth/signup', ctrlUser.userSign);
-router.post('/api/auth/login', ctrlUser.userLogin);
+router.post('/signup', ctrlUser.userSign);
+router.post('/login', ctrlUser.userLogin);
 
 module.exports = router;
