118c4894938a0d23b08566e51d942e8282e58bbc
VertoDebru <t.vervoot@gmail.com>
2022-01-19 13:45:58 +0100

    Debug images get.

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use('/api/auth/', userRoutes);
 app.use('/api/sauces', saucesRoutes);
-app.get('/images', express.static(path.join(__dirname, 'images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
