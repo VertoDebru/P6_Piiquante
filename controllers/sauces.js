992d32ab70968ff1a1bfe877cfccebb5fe5d736a
VertoDebru <t.vervoot@gmail.com>
2022-01-19 18:18:01 +0100

    Update controllers.

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const Sauces = require('../models/Sauces');
 
 exports.getAllSauces = (req,res) => {
@@ -7,31 +8,43 @@ exports.getAllSauces = (req,res) => {
 };
  
 exports.getSauce = (req,res) => {
-  const sauceId = req.path.split('/').join('');
+  const sauceId = req.params.id;
   Sauces.findById(sauceId)
   .then(sauce => res.status(200).json(sauce))
   .catch(error => res.status(400).json({ error }));
 };
 
 exports.editSauce = (req,res) => {
-  const sauceId = req.path.split('/').join('');
-  /*Sauces.findById(sauceId)
-  .then(sauce => res.status(200).json(sauce))
-  .catch(error => res.status(400).json({ error }));*/
+    const sauceObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+    Sauces.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteSauce = (req,res) => {
-  const sauceId = req.path.split('/').join('');
-  /*Sauces.findById(sauceId)
-  .then(sauce => res.status(200).json(sauce))
-  .catch(error => res.status(400).json({ error }));*/
+  Sauces.findOne({ _id: req.params.id })
+  .then(sauce => {
+    const filename = sauce.imageUrl.split('/images/')[1];
+    fs.unlink(`images/${filename}`, () => {
+      Sauces.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+    });
+  })
+  .catch(error => res.status(500).json({ error }));
 };
 
 exports.likeSauce = (req,res) => {
-  const sauceId = req.path.split('/').join('');
-  /*Sauces.findById(sauceId)
-  .then(sauce => res.status(200).json(sauce))
-  .catch(error => res.status(400).json({ error }));*/
+  const sauceId = req.params.id;
+  Sauces.updateOne({ _id: sauceId}, { 
+    ...req.body
+  })
+  .then(() => res.status(200).json({ message: 'Like enregistré !'}))
+  .catch(error => res.status(400).json({ error }));
 };
 
 exports.createSauce = (req,res) => {
--- routes/sauces.js
+++ routes/sauces.js
@@ -8,7 +8,7 @@ const ctrlSauces = require('../controllers/sauces');
 
 router.get('/', auth, ctrlSauces.getAllSauces);
 router.get('/:id', auth, ctrlSauces.getSauce);
-router.put('/:id', auth, ctrlSauces.editSauce);
+router.put('/:id', auth, multer, ctrlSauces.editSauce);
 router.delete('/:id', auth, ctrlSauces.deleteSauce);
 router.post('/:id/like', auth, ctrlSauces.likeSauce);
 router.post('/', auth, multer, ctrlSauces.createSauce);
