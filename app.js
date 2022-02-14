ad6d2d6989fcaa1109d57212d3f511329db7614c
VertoDebru <t.vervoot@gmail.com>
2022-02-14 16:27:05 +0100

    Security Methods

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const saucesRoutes = require('./routes/sauces');
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
 
     next();
